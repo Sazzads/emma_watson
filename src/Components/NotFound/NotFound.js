7eefea125b8358bf54e3598ca2bbfe605414da74
Sazzads <sazzadtk@gmail.com>
2022-05-04 14:12:06 +0600

    Not found image add

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './NotFound.css'
+import notFoundimg from '../../images/notFound.png'
 
 const NotFound = () => {
     const navigate = useNavigate();
@@ -8,11 +9,15 @@ const NotFound = () => {
         <div>
             <div className="container">
                 <div className='not-found-page'>
-                    <div>
-                        <h1>404</h1>
-                        <h2>Page Not Found!</h2>
-                        <p>The Page you are looking for doesn't exist or an other occurred, Go back.</p>
-                        <button onClick={() => navigate('/')}>Go Back</button>
+                    <div >
+                        <h1 className='text-danger mt-5 fourZeroFour'>404</h1>
+                        <h2>Page is Not Found!</h2>
+                        <img src={notFoundimg} alt="" />
+                        <p>The Page doesn't exist. Go back.</p>
+
+
+                        <button className='rounded-pill px-5 fw-bolder fs-6 mb-5 bg-warning' onClick={() => navigate('/')}>Go Back</button>
+                        <br />
                     </div>
                 </div>
             </div>
